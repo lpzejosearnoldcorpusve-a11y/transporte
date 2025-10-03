9038fbf6e0a5afab1cddcd0a3b472f8225d7f463
axrl79 <lpze.axelcristian.uria.fa@unifranz.edu.bo>
2025-10-03 14:44:47 -0400

    configurado vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
 import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next" 
 import { Suspense } from "react"
 import "./globals.css"
 
@@ -21,7 +22,12 @@ export default function RootLayout({
     <html lang="es">
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         <Suspense fallback={null}>{children}</Suspense>
+
+        {/* Analytics de Vercel */}
         <Analytics />
+
+        {/* Speed Insights de Vercel */}
+        <SpeedInsights />  {/* Speed Insights de Vercel */}
       </body>
     </html>
   )
