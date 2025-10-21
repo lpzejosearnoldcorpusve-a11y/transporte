521a52028cbea92f13e1c565ad4741cb19329b94
axrl79 <lpze.axelcristian.uria.fa@unifranz.edu.bo>
2025-10-21 00:01:50 -0400

    siempre hacer el build para ver que todo este bien , pndj

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server"
-import cloudinary from "@/lib/cloudinary"
+import { cloudinary } from "@/lib/cloudinary"
 import type { UploadApiResponse } from "cloudinary"
 
 export const runtime = "nodejs"
